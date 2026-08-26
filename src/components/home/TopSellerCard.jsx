f057127fcfbb37bda71d359bf4fe269ca726be43
Dmmiller1214 <dmmiller1214@gmail.com>
2026-08-26 15:44:11 -0400

    Add dynamic top sellers section

---
+++ src/components/home/TopSellerCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const TopSellerCard = ({ seller }) => {
+  return (
+    <li>
+      <div className="author_list_pp">
+        <Link to="/author">
+          <img
+            className="lazy pp-author"
+            src={seller.authorImage}
+            alt={seller.authorName}
+          />
+          <i className="fa fa-check"></i>
+        </Link>
+      </div>
+
+      <div className="author_list_info">
+        <Link to="/author">{seller.authorName}</Link>
+        <span>{Number(seller.price).toFixed(1)} ETH</span>
+      </div>
+    </li>
+  );
+};
+
+export default TopSellerCard;
